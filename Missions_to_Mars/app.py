a32c18956c684fdd8e3d67e0c95aed1f542e9b2f
Beth Myers <bmyers5817@gmail.com>
2021-03-22 20:45:56 -0400

    final update

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,15 +1,15 @@
 # import dependancies
 from flask import Flask, render_template, redirect
 from scrape_mars import Mars_Scraper
-# import pymongo 
-# conn = 'mongodb://localhost:27017'
-# client = pymongo.MongoClient(conn)
+from flask_pymongo import Pymongo 
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
 
-# # Declare the database
-# db = client.scraper_db
+# Declare the database
+db = client.scraper_db
 
-# # Declare the collection
-# collection = db.scraper_db
+# Declare the collection
+collection = db.scraper_db
 
 # Create an app, being sure to pass __name__
 app = Flask(__name__)
